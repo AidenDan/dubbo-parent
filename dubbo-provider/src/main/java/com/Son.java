1694cc5108f2413a0dec583e3f6ee4e80c260d8f
AidenDan <1299930560@qq.com>
2020-05-03 21:45:25 +0800

    123

---
+++ dubbo-provider/src/main/java/com/Son.java
@@ -0,0 +1,17 @@
+package com;
+
+/**
+ * @author Aiden
+ * @version 1.0
+ * @description
+ * @date 2020-5-3 20:56:55
+ */
+public class Son {
+    public static void main(String[] args) {
+        System.out.println("测试用。这是主分支");
+        System.out.println("测试用。这是主分支");
+        System.out.println("测试用。这是主分支");
+        System.out.println("测试用。这是主分支");
+        System.out.println("测试用。这是主分支");
+    }
+}
